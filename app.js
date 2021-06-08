c9c6e66effd77081df692c61e381700a68050e0d
alejRes <alejrespal@gmail.com>
2021-06-08 17:23:22 +0200

    create files

---
+++ app.js
@@ -0,0 +1,17 @@
+const port = process.env.PORT || 3000
+
+const express = require('express')
+
+require('dotenv').config()
+
+app.use("/public", express.static("public"))
+
+app.set('view engine','pug')
+app.set('views','./views')
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(port, ()=>{
+    console.log(`servidor: http://localhost:${port}`)
+})

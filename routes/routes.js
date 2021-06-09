4e8f86fcbf0396b59ab10da6618a44cf116af0cb
VictorMaVargas <victormavargas@gmail.com>
2021-06-09 15:51:38 +0200

    rutas en un archivo

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
 const router = require ("express").Router();
 const user = require ("./controllers/user")
+const admin = require ("./controllers/admin")
+//const auth = require ("./controllers/auth")
 
 router.get('/', user.home)
 router.get('/dashboard', user.dashboard)
@@ -10,6 +12,12 @@ router.get('/movies', user.movies)
 router.post('/signup', user.signup)
 router.post('/login', user.login)
 router.post('/logout', user.logout)
+
+router.post('/login', admin.login)
+router.post('/createMovie', admin.createMovie)
+//router.get('/movies', admin.movies)
+router.put('/editMovie/:id', admin.editMovie)
+router.delete('/removeMovie', admin.removeMovie)
 
 router.get('*', user.home)
 

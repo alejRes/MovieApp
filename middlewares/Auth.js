0efa5999cb555ee5c725a88c867c0cdd8094fb71
AntonioAp <antonioaparicioh@gmail.com>
2021-06-22 16:02:03 +0200

    css styles finished and add login spinner

--- middlewares/Auth.js
+++ middlewares/Auth.js
@@ -6,7 +6,7 @@ function isAuth(req, res, next) {
   if (!req.headers.authorization) {
     return res.status(403).send({ message: "Prohibido el acceso" }); //código de prohibido el acceso
   }
-  const token = req.headers.authorization.split(" ")[1]; //aquí le decimos que nos coja el token de las cabeceras y que nos cree un array deglosandolo con el método split y
+  const token = req.headers.authorization.split(" ")[1]; //aquí le decimos que nos coja el token de las cabeceras y que nos cree un array desglosandolo con el método split y
   //como está formado por un texto que pone bearer + token le decimos que queremos la posic.1 porque es la que corresponde al token
 
   services.decodeToken(token);
